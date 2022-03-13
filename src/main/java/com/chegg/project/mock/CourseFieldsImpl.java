556c5ddd7c32417c50fb64aa8ba13f6e75cab05c
Alex Bakshi Richardson <hidden>
2022-03-13 12:24:01 -0700

    Moved everything for more standard directory structure.  Also fixed a compile error.

--- src/main/java/com/chegg/project/mock/CourseFieldsImpl.java
+++ src/main/java/com/chegg/project/mock/CourseFieldsImpl.java
@@ -1,8 +1,11 @@
 package com.chegg.project.mock;
 
+import com.chegg.project.EntityType;
+
 public class CourseFieldsImpl extends EntityFieldsImpl {
 
 	public CourseFieldsImpl(CourseFieldsImpl courseFields) {
+		super(EntityType.COURSE);
 		this.setAllFields(courseFields.getAllFields());
 	}
 
---
+++ src/test/java/com/chegg/project/mock/CourseFieldsImplTest.java
@@ -0,0 +1,44 @@
+package com.chegg.project.mock;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CourseFieldsImplTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void testGetType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAllFields() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetField() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetField() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetAllFields() {
+		fail("Not yet implemented");
+	}
+
+}
