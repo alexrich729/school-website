785e950213fbf8cadb25481b291c2f1eed054fd4
Alex Bakshi Richardson <hidden>
2022-03-13 14:03:56 -0700

    Made EntityImpl instantiable.

--- src/main/java/com/chegg/project/Config.java
+++ src/main/java/com/chegg/project/Config.java
@@ -3,12 +3,12 @@ package com.chegg.project;
 import java.util.List;
 
 public class Config {
-    public static List<Field> getRequiredFields(EntityType type) {
+    public List<Field> getRequiredFields(EntityType type) {
         //TODO
         return null;
     }
 
-    public static List<Field> getOptionalFields(EntityType type) {
+    public List<Field> getOptionalFields(EntityType type) {
         //TODO
         return null;
     }
