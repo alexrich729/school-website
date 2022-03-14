d4da99ea10083f52f005da37607eb5d9620c6851
Alex Bakshi Richardson <hidden>
2022-03-14 07:56:18 -0700

    Setup mock data for manager test.

--- src/test/java/com/chegg/project/mock/ManagerImplTest.java
+++ src/test/java/com/chegg/project/mock/ManagerImplTest.java
@@ -19,19 +19,18 @@ public class ManagerImplTest {
 		List<User> users = new ArrayList<>();
 		List<Course> courses = new ArrayList<>();
 		List<School> schools = new ArrayList<>();
-		for (int i = 0; i < 26; i++) {
-			List<Field> fields = new ArrayList<>();
-			fields.add(new Field("name", Character.toString((char) ('A' + i)), FieldType.STRING, true, null));
-			fields.add(new Field("email", Character.toString((char) ('A' + i)) + "@email.com", FieldType.STRING, true, null));
-			if (i % 2 == 0) {
-				fields.add(new Field("isStudent", true, FieldType.BOOLEAN, null, null));
-			} else {
-				fields.add(new Field("isProfessor", true, FieldType.BOOLEAN, null, null));
-			}
-			users.add(new UserImpl(EntityType.USER, fields, config))
+		// creates dummy data to start off with
+		for (int i = 0; i < 100; i++) {
+			EntityFieldsImpl schoolFields = new EntityFieldsImpl(EntityType.SCHOOL, config);
+			EntityFieldsImpl courseFields = new EntityFieldsImpl(EntityType.COURSE, config);
+			EntityFieldsImpl userFields = new EntityFieldsImpl(EntityType.USER, config);
+			schools.add(createNewEntity(schoolFields).buildSchool());
+			courses.add(createNewEntity(courseFields).buildCourse());
+			users.add(createNewEntity(userFields).buildUser());
+
 		}
 		
-		Manager manager = new ManagerImpl();
+		Manager manager = new ManagerImpl(users, courses, schools);
 
 	}
 
@@ -39,9 +38,40 @@ public class ManagerImplTest {
 	public static void tearDownAfterClass() throws Exception {
 	}
 	
-	public static Entity createNewEntity(EntityFields ef) {
-		String name = (char) ('A' + (int)( Math.random() * 26.0)) + "" + (char) ('a' + (int)( Math.random() * 26.0)) + "" + (char) ('a' + (int)( Math.random() * 26.0));
-		ef.setField("name", ef);
+	public static Entity createNewEntity(EntityFieldsImpl ef) {
+		double rand1 = Math.random();
+		double rand2 = Math.random();
+		double rand3 = Math.random();
+
+		String name = (char) ('A' + (int)( rand1 * 26.0)) + "" + (char) ('a' + (int)( rand2 * 26.0)) + "" + (char) ('a' + (int)( rand3 * 26.0));
+		ef.setField("name", name);
+		
+		
+		if (ef.getType() == EntityType.SCHOOL) {
+			Entity schoolEntity = new EntityImpl(ef);
+			return schoolEntity.buildSchool();
+		} else if (ef.getType() == EntityType.COURSE) {
+			if (rand2 >= 0.5)
+				ef.setField("school", name);
+			Entity courseEntity = new EntityImpl(ef);
+			return courseEntity.buildCourse();
+		} else if (ef.getType() == EntityType.USER) {
+			ef.setField("email", name + "@email");
+			if (rand1 >= 0.5)
+				ef.setField("isStudent", true);
+			else
+				ef.setField("isTeacher", true);
+			
+			if (rand3 >= 0.5) {
+				EntityFieldsImpl schoolFields = new EntityFieldsImpl(EntityType.SCHOOL, ef.getConfig());
+				schoolFields.setField("name", name);
+				Entity schoolEntity = new EntityImpl(schoolFields);
+				School school = schoolEntity.buildSchool();
+				ef.setField("school", school);
+			}
+			Entity userEntity = new EntityImpl(ef);
+			return userEntity.buildUser();
+		}
 		return null;
 	}
 
